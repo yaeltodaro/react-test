9454cb5997df852716506709b431fe199007d3f6
Yael <y.todaro@gmail.com>
2023-03-03 17:21:51 +0100

    Arreglo de rutas

--- src/components/router.js
+++ src/components/router.js
@@ -12,10 +12,10 @@ import { Routes ,Route, Navigate } from 'react-router-dom';
 const Router = () => (
   <BrowserRouter basename="/react-test/">
      <Routes>
-       <Route path='/' element={<App/>} />
+       <Route path='/app' element={<App/>} />
        <Route path="/platillos" element={<Dishes/>} />
        <Route path="/platillo/:name" element={<Dish/>} />
-       <Route path="/react-test/paises" element={<Countries/>} />
+       <Route path="/paises" element={<Countries/>} />
        <Route path="*" element={<Navigate to="/" />} />
        <Route component={NotFound} />
      </Routes>
